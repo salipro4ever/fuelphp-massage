a0edf48a22a2ab636245fec811c9be55c24065c4
SaliproPham <sangph@suga.vn>
2017-12-08 18:30:54 +0700

    init

---
+++ config/messages.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * FuelPHP Messages
+ * "MIT License"
+ * Copyright 2013 Michiel Hendriks <elmuerte@drunksnipers.com>
+ */
+
+return array (
+		/**
+		 * If true, clear the messages when calling the get methods.
+		 */
+		'allow_type' => array(
+            'error'   => 'alert-danger',
+            'info'    => 'alert-info',
+            'warning' => 'alert-warning',
+            'success' => 'alert-success',
+        ),
+);
